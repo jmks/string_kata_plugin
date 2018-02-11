cf680339c699eab974a1e89d1adc1277e7e153d4
Jason Schweier <jasons@nulogy.com>
2018-02-10 23:49:03 -0500

    Prepatory refactor to calculate the delimiter

--- lib/string_kata_plugin/string_calculator.rb
+++ lib/string_kata_plugin/string_calculator.rb
@@ -5,10 +5,16 @@ module StringKataPlugin
     def add(numbers)
       return 0 if numbers.empty?
 
-      numbers
-        .split(/[,\s]/)
+      delimiter, only_numbers = extract_delimiter(numbers)
+
+      only_numbers
+        .split(delimiter)
         .map(&:to_i)
         .reduce(0, &:+)
+    end
+
+    def extract_delimiter(numbers)
+      [/[,\s]/, numbers]
     end
   end
 end
